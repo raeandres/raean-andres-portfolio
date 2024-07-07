0c8eb47f7f00a17ff54ac02b1cc4c144aef52b98
Rae-An Andres <raeandres@users.noreply.github.com>
2024-07-07 19:38:20 +0800

    Mobile Nav Updates

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -38,10 +38,23 @@ const MobileNav = () => {
         </SheetTrigger>
         <SheetContent className='flex flex-col'>
             {/* Logo */}
-            <div>
-            Logo
+            <div className='mt-32 mb-40 text-center text-2xl'>
+                <Link href="/">
+                    <h1 className='text-4xl font-semibold'>Rae-an<span className='text-accent'>.</span></h1>
+                </Link>
             </div>
-            </SheetContent>
+            <nav className='flex flex-col justify-center items-center gap-8'>
+                {links.map((link, index) => {
+                    return (
+                        <Link 
+                        href={link.path} 
+                        key={index} 
+                        className={`${link.path === pathName && 'text-accent border-b-2 border-accent'}text-xl capitalize hover:text-accent transition-all`}>
+                        {link.name}</Link>
+                    )
+                })}
+            </nav>
+        </SheetContent>
     </Sheet>
 }
 
